c4ace28bdf6f102653b03481564d1cf44556dbd0
Jonas Thuvesson Winberg <jonasthuvesson@gmail.com>
2022-11-23 21:25:05 +0100

    Improve

--- lib/elixir_cli_tool_template.ex
+++ lib/elixir_cli_tool_template.ex
@@ -1,10 +1,18 @@
 defmodule ElixirCliToolTemplate do
   def main(args) do
-    {parsed, _argv, errors} = OptionParser.parse(args, strict: [example: :string])
+    {parsed, _argv, errors} =
+      OptionParser.parse(args, strict: [greet: :string, a_number: :integer])
 
     if parsed !== [] do
-      [example: x] = parsed
-      IO.puts("Hello #{x}")
+      for command <- parsed do
+        res =
+          case command do
+            {:greet, v} -> "Hello #{v}"
+            {:a_number, v} -> "This is a number: #{v}"
+          end
+
+        IO.puts(res)
+      end
     else
       IO.puts("Hello World")
     end
@@ -12,7 +20,7 @@ defmodule ElixirCliToolTemplate do
     if errors !== [] do
       for {key, value} <- errors do
         if value do
-          IO.puts("Incorrect arg \"#{key}\" with value \"#{value}\"")
+          IO.puts("Incorrectly used known arg \"#{key}\" with value \"#{value}\"")
         else
           IO.puts("Incorrect arg \"#{key}\"")
         end
