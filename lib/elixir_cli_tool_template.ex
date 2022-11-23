073e48458ba4f2e500cfd0127e52af4a814f21fe
Jonas Thuvesson Winberg <jonasthuvesson@gmail.com>
2022-11-23 20:44:48 +0100

    Change base logic, and remove compiled tool

--- lib/elixir_cli_tool_template.ex
+++ lib/elixir_cli_tool_template.ex
@@ -2,16 +2,21 @@ defmodule ElixirCliToolTemplate do
   def main(args) do
     {parsed, _argv, errors} = OptionParser.parse(args, strict: [example: :string])
 
-    cond do
-      parsed !== [] ->
-        [example: x] = parsed
-        IO.puts("Hello #{x}")
-      errors !== [] ->
-        for {key, value} <- errors do
+    if parsed !== [] do
+      [example: x] = parsed
+      IO.puts("Hello #{x}")
+    else
+      IO.puts("Hello World")
+    end
+
+    if errors !== [] do
+      for {key, value} <- errors do
+        if value do
           IO.puts("Incorrect arg \"#{key}\" with value \"#{value}\"")
+        else
+          IO.puts("Incorrect arg \"#{key}\"")
         end
-      true ->
-        IO.puts("Hello World")
+      end
     end
   end
 end
